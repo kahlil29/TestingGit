e46916546c1477ab6970a0edeaababaa9be8f1ed
shubhamkesarkar <shubhamkesarkar@gmail.com>
2015-07-05 20:33:35 +0530

    Python code using Dulwich to initialize

--- Test.py
+++ Test.py
@@ -1,3 +1,15 @@
-printf("Yo")
-print("hello world")
-print("Just a change")
+from dulwich.repo import Repo
+from os import mkdir
+import os
+os.chdir("/home/shubham/Documents")
+mkdir("dulwichtest")
+repo = Repo.init("dulwichtest")
+repo
+index = repo.open_index()
+repr(index).replace('\\\\', '/')
+list(index)
+f = open('dulwichtest/foo', 'w')
+f.write("monty")
+f.close()
+repo.stage(["foo"])
+
