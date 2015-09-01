5c3bc2f1cf602e117b727fea7ce86451f3f15971
Kahlil <kahlil29@gmail.com>
2015-09-02 01:08:44 +0530

    commit to git staging area

--- shelltestinggit.py
+++ shelltestinggit.py
@@ -1,19 +1,20 @@
 import subprocess
 import os
-
-
-pwd = subprocess.Popen("pwd", shell=True, stdout=subprocess.PIPE).stdout.read()
 print pwd
-cwd = pwd[:-1]	#get the directory without file name and extra new line char to use in popen
-pwd = cwd
-print cwd
-print subprocess.Popen("git status", cwd = cwd, shell=True, stdout=subprocess.PIPE).stdout.read()
-current_file = str(os.path.abspath(__file__))		#directory including filename for add
-print subprocess.Popen("git add "+current_file, cwd = pwd, shell=True, stdout=subprocess.PIPE).stdout.read()
-print subprocess.Popen("git status", cwd = pwd, shell=True, stdout=subprocess.PIPE).stdout.read()
-print subprocess.Popen("git commit -m\"commit to git staging area\"", cwd = pwd, shell=True, stdout=subprocess.PIPE).stdout.read()
-print subprocess.Popen("git pull origin master", cwd = pwd, shell=True, stdout=subprocess.PIPE).stdout.read()
-print subprocess.Popen("git push origin master", cwd = cwd, shell=True, stdout=subprocess.PIPE).stdout.read()
+#print subprocess.Popen("git push origin master", cwd = , shell=True, stdout=subprocess.PIPE).stdout.read()
+
+# pwd = subprocess.Popen("pwd", shell=True, stdout=subprocess.PIPE).stdout.read()
+# print pwd
+# cwd = pwd[:-1]	#get the directory without file name and extra new line char to use in popen
+# pwd = cwd
+# print cwd
+# print subprocess.Popen("git status", cwd = cwd, shell=True, stdout=subprocess.PIPE).stdout.read()
+# current_file = str(os.path.abspath(__file__))		#directory including filename for add
+# print subprocess.Popen("git add "+current_file, cwd = pwd, shell=True, stdout=subprocess.PIPE).stdout.read()
+# print subprocess.Popen("git status", cwd = pwd, shell=True, stdout=subprocess.PIPE).stdout.read()
+# print subprocess.Popen("git commit -m\"commit to git staging area\"", cwd = pwd, shell=True, stdout=subprocess.PIPE).stdout.read()
+# print subprocess.Popen("git pull origin master", cwd = pwd, shell=True, stdout=subprocess.PIPE).stdout.read()
+# print subprocess.Popen("git push origin master", cwd = cwd, shell=True, stdout=subprocess.PIPE).stdout.read()
 
 
 
